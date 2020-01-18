0ed65c03050fdc2885c265686c314478c6ff5b0d
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-01-18 22:41:35 +0100

    feat: setup basic carousel

--- playground/SimpleCarousel.story.tsx
+++ playground/SimpleCarousel.story.tsx
@@ -1,17 +1,19 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
+import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types'
+
 import ReactPanoply from 'react-panoply'
 
 const slides = new Array(10).fill(0).map((item, index) => (
-  <div key={index} style={{ height: 200 }}>
+  <div key={index} style={{ height: 200, background: '#f2f2f2' }}>
     slide {index + 1}
   </div>
 ))
 
-const SimpleCarouselStory = (): ReactNode => {
+const SimpleCarouselStory = (): StoryFnReactReturnType => {
   return (
     <ReactPanoply
       params={{
-        slidesPerView: 8,
+        slidesPerView: 3,
         slidesSpacing: 20,
       }}
     >
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React, { Children, FunctionComponent, ReactNode, CSSProperties } from 'react'
+import React, { Children, FunctionComponent, ReactNode, CSSProperties, useState } from 'react'
 
 import { REACT_PANOPLY_CLASS, REACT_PANOPLY_WRAPPER_CLASS } from './constants'
 
 export interface ReactPanoplyParams {
   slidesSpacing: number
   slidesPerView: 'auto' | number
+  initialSlide: number
+  loop: boolean
+}
+
+export interface ReactPanoplyParsedParams extends ReactPanoplyParams {
+  slidesPerView: number
 }
 
 export interface ReactPanoplyProps {
@@ -14,24 +20,58 @@ export interface ReactPanoplyProps {
   wrapperClass?: string
 }
 
-type GetCarouselWrapperStyles = (slidesLength: number, slidesSpacing: number, slidesPerView: number) => CSSProperties
+export interface ReactPanoplyState {
+  activeSlideIndex: number
+}
 
-const getCarouselWrapperStyles: GetCarouselWrapperStyles = (slidesLength, slidesSpacing, slidesPerView) => {
-  const gridWrapperWidth = (slidesLength * 100) / slidesPerView
-  const gridSlidesSpacingOffset = slidesSpacing * ((gridWrapperWidth - 100) / 100)
+type GetCarouselWrapperStyles = (
+  slidesLength: number,
+  params: ReactPanoplyParsedParams,
+  activeSlideIndex: number,
+) => CSSProperties
 
+const clamp = (num, min, max) => (num <= min ? min : num >= max ? max : num)
+
+const getCarouselWrapperStyles: GetCarouselWrapperStyles = (slidesLength, params, activeSlideIndex) => {
+  const { slidesSpacing, slidesPerView } = params
+  const gridWrapperPercentWidth = (slidesLength * 100) / slidesPerView
+  const gridSlidesSpacingOffset = slidesSpacing * ((gridWrapperPercentWidth - 100) / 100)
   return {
     display: 'grid',
     gridTemplateColumns: `repeat(${slidesLength}, 1fr)`,
     gridTemplateRows: `1fr`,
     gridColumnGap: slidesSpacing,
-    width: `calc(${gridWrapperWidth}% + ${gridSlidesSpacingOffset}px)`,
+    width: `calc(${gridWrapperPercentWidth}% + ${gridSlidesSpacingOffset}px)`,
+    transform: `translateX(-${(activeSlideIndex * 100) / slidesLength}%)`,
+    transition: 'transform 300ms',
   }
 }
 
 const defaultParams: ReactPanoplyParams = {
   slidesSpacing: 10,
   slidesPerView: 'auto',
+  initialSlide: 1,
+  loop: false,
+}
+
+const defaultState: ReactPanoplyState = {
+  activeSlideIndex: 0,
+}
+
+const nextSlide = (setActiveSlideIndex, params, slidesLength) => {
+  if (params.loop) {
+    setActiveSlideIndex(prev => prev + 1)
+  } else {
+    setActiveSlideIndex(prev => clamp(prev + 1, 0, slidesLength - params.slidesPerView))
+  }
+}
+
+const prevSlide = (setActiveSlideIndex, params, slidesLength) => {
+  if (params.loop) {
+    setActiveSlideIndex(prev => prev - 1)
+  } else {
+    setActiveSlideIndex(prev => clamp(prev - 1, 0, slidesLength - params.slidesPerView))
+  }
 }
 
 const ReactPanoply: FunctionComponent<ReactPanoplyProps> = ({
@@ -40,16 +80,24 @@ const ReactPanoply: FunctionComponent<ReactPanoplyProps> = ({
   params = {},
   wrapperClass = REACT_PANOPLY_WRAPPER_CLASS,
 }) => {
-  const mergedParams = { ...defaultParams, ...params }
   const slidesLength = Children.count(children)
-  const normalizedSlidesPerView = mergedParams.slidesPerView === 'auto' ? slidesLength : mergedParams.slidesPerView
-  const wrapperStyles = getCarouselWrapperStyles(slidesLength, mergedParams.slidesSpacing, normalizedSlidesPerView)
+  const mergedParams = { ...defaultParams, ...params }
+  const parsedParams = {
+    ...mergedParams,
+    slidesPerView: mergedParams.slidesPerView === 'auto' ? slidesLength : mergedParams.slidesPerView,
+  }
+
+  const [activeSlideIndex, setActiveSlideIndex] = useState(params.initialSlide ?? defaultState.activeSlideIndex)
+  const wrapperStyles = getCarouselWrapperStyles(slidesLength, parsedParams, activeSlideIndex)
 
   return (
     <div style={{ width: '100%', overflow: 'hidden' }} className={className}>
       <div style={wrapperStyles} className={wrapperClass}>
         {children}
       </div>
+
+      <button onClick={() => prevSlide(setActiveSlideIndex, parsedParams, slidesLength)}> prev </button>
+      <button onClick={() => nextSlide(setActiveSlideIndex, parsedParams, slidesLength)}> next </button>
     </div>
   )
 }
