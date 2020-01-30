654a6002fa8579ea119674bfd2c42eb0f58c0df0
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-01-31 00:26:42 +0100

    refactor: stop parsing initialSlide param

--- src/helpers/parseParams/parseParams.helper.ts
+++ src/helpers/parseParams/parseParams.helper.ts
@@ -4,7 +4,6 @@ const parseParams: ParseParamsHelper = (params, slidesLength) => {
   return {
     ...params,
     slidesPerView: params.slidesPerView === 'auto' ? slidesLength : params.slidesPerView,
-    initialSlide: params.initialSlide - 1,
   }
 }
 
